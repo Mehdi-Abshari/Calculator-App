ccde7b4294da8071d1d1dd099998184bf218b83e
Mehdi-Abshari <absharimahdi5@gmail.com>
2023-03-14 13:38:40 +0330

    restore HTML and js code
    کد های HTML و js از اول زده شد.

--- script.js
+++ script.js
@@ -1,93 +1,67 @@
-// --> Varibales
-// Selecting body
-let body = document.querySelector("body"),
-  // Selecting answer input
-  answerInput = document.querySelector("#answerInput"),
-  // Selecting all buttons in calculator
-  allBtns = document.querySelectorAll(".buttons"),
-  // Selecting delete all button
-  deleteAllBtn = document.querySelector(".delete-all"),
-  // Selecting delete last button
-  deleteLastBtn = document.querySelector("#delete-last"),
-  // Selecting all numbers in calculator
-  numbers = document.querySelectorAll(".numbers"),
-  // Selecting equal button in calculator
-  equalBtn = document.querySelector("#equal"),
-  // Selecting math operator buttons
-  mathOperators = document.querySelectorAll(".math-operators"),
-  // Selecting advanced calculation buttons
-  advancedCalculation = document.querySelectorAll(".advanced-calculation"),
-  // Selecting separators numbers
-  numberSeparators = document.querySelectorAll(".number-separators");
+// --> Variables
+// Selecting the textarea for perform calculation
+let screen = document.querySelector("#screen");
 
-// --> Events
-// Delete delete All Button
-deleteAllBtn.addEventListener("click", () => {
-  deleteAllBtnValue("");
-});
-// Delete last button ( <- )
-deleteLastBtn.addEventListener("click", deleteLastInputValue);
-// equal Button (=)
-equalBtn.addEventListener("click", accountAnswer);
+// Selecting All button of calculator
+let btn = document.querySelectorAll(".btn");
+
+// --> Loops
+// loop for add event to button to check the innerText of buttons
+for (item of btn) {
+  // Event
+  item.addEventListener("click", (e) => {
+    btnText = e.target.innerText;
+
+    // check for multiplication
+    if (btnText == "×") {
+      btnText = "*";
+    }
+
+    // check for division
+    if (btnText == "÷") {
+      btnText = "/";
+    }
+    screen.value += btnText;
+  });
+}
 
 // --> Functions
-// Function for when click at numbers button
-function chooseNumber() {
-  // Normal numbers
-  numbers.forEach((item) => {
-    item.addEventListener("click", () => {
-      addToDisplay(item.textContent);
-      count = 0;
-    });
-  });
-  // '(' , ')' , '.' numbers
-  numberSeparators.forEach((item) => {
-    item.addEventListener("click", () => {
-      if (item.textContent == "(" || item.textContent == ")") {
-        addToDisplay(item.textContent);
-      } else if (
-        count < 1 &&
-        item.textContent == "." &&
-        answerInput.value != ""
-      ) {
-        addToDisplay(item.textContent);
-        count++;
-      }
-    });
-  });
+// function for get sine
+function sin() {
+  screen.value = Math.sin(screen.value);
 }
 
-// Function for chose data value of math operators
-function chooseOperator() {
-  mathOperators.forEach((item) => {
-    item.addEventListener("click", () => {
-      if (
-        count < 1 &&
-        answerInput.value != "" &&
-        item.getAttribute("data-value") != "-"
-      ) {
-        addToDisplay(item.getAttribute("data-value"));
-        count++;
-      } else if (count < 1 && item.getAttribute("data-value") == "-") {
-        addToDisplay(item.textContent);
-        count++;
-      }
-    });
-  });
+// function for get cosine
+function cos() {
+  screen.value = Math.cos(screen.value);
 }
 
-// Function for adds the argument to answerInput.value + any thing that was already in the textarea
-function addToDisplay(button) {
-  answerInput.value += button;
+// function for get tangent
+function tan() {
+  screen.value = Math.tan(screen.value);
 }
 
-// Function for adds the argument to answerInput.value
-function deleteAllBtnValue(name) {
-  answerInput.value = name;
+// function for get cotangent
+function cot() {
+  screen.value = Math.cot(screen.value);
 }
 
-// Function for slice answerInput in deleteAllBtnValue
-function deleteLastInputValue() {
-  deleteAllBtnValue(answerInput.value.slice(0, -1));
+// function for get logarithm
+function log() {
+  screen.value = Math.log(screen.value);
 }
 
+// function for get radical
+function sqrt() {
+  screen.value = Math.sqrt(screen.value, 2);
+}
+
+// function for get pi (π)
+function pi() {
+  screen.value = 3.14159265359;
+}
+
+// function for backSpace textarea length
+function backspc() {
+  screen.value = screen.value.substr(0, screen.value.length - 1);
+}
